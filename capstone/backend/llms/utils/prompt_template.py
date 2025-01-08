38a3e33b2ef19838e7b989a8844dd6974476a3b7
Santapong Sondhi <santapongsondhi@gmail.com>
2025-01-08 22:56:46 +0700

    add langgraph for orchestration

--- capstone/backend/llms/utils/prompt_template.py
+++ capstone/backend/llms/utils/prompt_template.py
@@ -1,3 +1,4 @@
 from langchain_core.prompts import (PromptTemplate, 
                                     SystemMessagePromptTemplate, 
                                     HumanMessagePromptTemplate)
+
