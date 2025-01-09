8b3f45890605bd259d70eed28005769114460397
LAPTOP-UFCHV9CV\User <jobnakubphom@gmail.com>
2025-01-09 16:32:11 +0700

    adjust Embedding

--- capstone/backend/llms/embedding/embedding_ollama.py
+++ capstone/backend/llms/embedding/embedding_ollama.py
@@ -1,1 +1,41 @@
-from langchain_ollama import OllamaEmbeddings
+import logging
+
+from dataclasses import dataclass
+
+from langchain_chroma import Chroma
+from langchain_ollama.embeddings import OllamaEmbeddings
+
+
+@dataclass
+class OllamaEmbedder:
+    model: str = None
+    """_summary_
+    """
+    
+    path: str = None
+    """_summary_
+    """ 
+
+    collection_name: str = None
+    """_summary_
+    """
+    
+    def __post_init__(self):
+        logging.info("Connect to LLAMA3.2 embedding")
+        print()
+        self.embeddings = OllamaEmbeddings(model='llama3.2')
+
+    def store(self):
+        logging.info()
+        self.vector_store = Chroma(
+            collection_name="example_collection",
+            embedding_function=self.embeddings,
+            persist_directory="./chroma_langchain_db",  # Where to save data locally, remove if not necessary
+        )
+    
+    def collection(self):
+        pass
+        
+if __name__ == '__name__':
+    embeddings = OllamaEmbedder(model='llama3.2')
+    embeddings.store()
