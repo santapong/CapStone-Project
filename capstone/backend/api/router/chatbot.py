7139980b500d45677cbd086c184c8c0de446b306
Santapong Sondhi <santapongsondhi@gmail.com>
2025-01-14 19:41:56 +0700

    update log

--- capstone/backend/api/router/chatbot.py
+++ capstone/backend/api/router/chatbot.py
@@ -1,16 +1,25 @@
+import time
+import logging
 from fastapi import APIRouter
 
 from capstone.backend.llms.main import ChatModel
+
+logging.getLogger(__name__)
 
 tags = ["Chatbot"]
 router_chatbot = APIRouter(prefix='/chatbot')
 
 @router_chatbot.get("/infer", tags=tags)
 async def inferenceModel(question):
+    start_time = time.time()
     chatbot = ChatModel()
+    answer = chatbot.query(question=question)
+    time_usage = time.time()-start_time
+    logging.info(time_usage)
     return {
+        "time usage": time_usage,
         "question": question,
-        "answer": chatbot.query(question=question)
+        "answer": answer
         }
 
 @router_chatbot.get("/infer/{model}", tags=tags)
--- capstone/backend/app.py
+++ capstone/backend/app.py
@@ -1,11 +1,24 @@
 import uvicorn
+import logging
 
 from fastapi import FastAPI
+
 from capstone.backend.api.router.chatbot import router_chatbot
 from capstone.backend.api.router.dashboard import router_dashboard
 from capstone.backend.api.router.document import router_document
 
+from capstone.backend.logs.logging_config import setup_logging 
+
+# Setup logging for more security.
+setup_logging()
+logging.getLogger(__name__)
+
 app = FastAPI()
+
+@app.get('/tests')
+def test():
+    logging.info("Hello")
+
 app.include_router(router_chatbot)
 app.include_router(router_dashboard)
 app.include_router(router_document)
