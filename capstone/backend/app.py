e2520d95cb98a10034ef9ddcecb8b7b1281178e6
Santapong Sondhi <santapongsondhi@gmail.com>
2025-01-14 22:34:14 +0700

    create dynamic API

--- capstone/backend/app.py
+++ capstone/backend/app.py
@@ -1,13 +1,18 @@
+import os
 import uvicorn
 import logging
 
 from fastapi import FastAPI
+from dotenv import load_dotenv
 
+from capstone.backend.logs.logging_config import setup_logging
+
+from capstone.backend.api.router.dynamic import router_dynamic
 from capstone.backend.api.router.chatbot import router_chatbot
-from capstone.backend.api.router.dashboard import router_dashboard
 from capstone.backend.api.router.document import router_document
+from capstone.backend.api.router.dashboard import router_dashboard
 
-from capstone.backend.logs.logging_config import setup_logging 
+load_dotenv()
 
 # Setup logging for more security.
 setup_logging()
@@ -15,14 +20,10 @@ logging.getLogger(__name__)
 
 app = FastAPI()
 
-@app.get('/tests')
-def test():
-    logging.info("Hello")
-
+app.include_router(router_dynamic)
 app.include_router(router_chatbot)
 app.include_router(router_dashboard)
 app.include_router(router_document)
-
 
 if __name__ == '__main__':
     uvicorn.run('app:app', reload=True)
