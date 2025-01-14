a451e3d5e7a145ea6e165ecc0546a404b5db19e0
Santapong Sondhi <santapongsondhi@gmail.com>
2025-01-14 14:42:44 +0700

    create main

---
+++ capstone/backend/llms/core/main.py
@@ -0,0 +1,17 @@
+from capstone.backend.llms.core.models import ChatModel
+from capstone.backend.llms.vectordb.session import VectorDBConnect
+from capstone.backend.llms.loadder import (WebLoaderManager,
+                                           LoadPDFManager,
+                                           LoadTextManager)
+
+from capstone.backend.llms.utils.prompt_template import (few_shot_prompt_template,
+                                                         few_shot_input_variable,
+                                                         test_prompt_template,
+                                                         test_prompt_input_variable)
+
+from capstone.backend.llms.utils.splitter import SplitterManager
+
+model = ChatModel()
+print(model.query("Hello can you introduce your self."))
+
+
