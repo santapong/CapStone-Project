642a8cab0e2f7db9f8930c14a8e5c905c3fd442f
Santapong Sondhi <santapongsondhi@gmail.com>
2025-01-15 22:27:18 +0700

    make logging will run when import

--- capstone/backend/app.py
+++ capstone/backend/app.py
@@ -3,24 +3,25 @@ import uvicorn
 import logging
 
 from fastapi import FastAPI
+from fastapi.responses import UJSONResponse
 from dotenv import load_dotenv
-
-from capstone.backend.api.database import DBConnect
 
 from capstone.backend.api.router.dynamic import router_dynamic
 from capstone.backend.api.router.chatbot import router_chatbot
 from capstone.backend.api.router.document import router_document
 from capstone.backend.api.router.dashboard import router_dashboard
 
+# It already use when import.
+from capstone.backend.api.database import DBConnect
 from capstone.backend.logs.logging_config import setup_logging
 
 load_dotenv()
 
-# Setup logging for more security.
-setup_logging()
 logging.getLogger(__name__)
 
-app = FastAPI()
+prefix = os.getenv("PATH_PREFIX", default='/')
+
+app = FastAPI(prefix=prefix)
 
 app.include_router(router_dynamic)
 app.include_router(router_chatbot)
