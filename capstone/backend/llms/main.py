3f3ac4d767e09a8edc62f30cb4271605f1babfae
Santapong Sondhi <santapongsondhi@gmail.com>
2025-01-16 11:12:57 +0700

    update project stucture

--- capstone/backend/llms/main.py
+++ capstone/backend/llms/main.py
@@ -1,13 +1,62 @@
+import os 
+import logging
+
+from dotenv import load_dotenv
+from abc import abstractmethod, ABC
+
 from capstone.backend.llms.core.models import ChatModel
 from capstone.backend.llms.vectordb.session import VectorDBConnect
 
-from capstone.backend.llms.utils.prompt_template import (few_shot_prompt_template,
-                                                         few_shot_input_variable,
-                                                         test_prompt_template,
-                                                         test_prompt_input_variable)
+from capstone.backend.llms.prompt_teamplate.prompt_template import (few_shot_prompt_template,
+                                                                    few_shot_input_variable,
+                                                                    test_prompt_template,
+                                                                    test_prompt_input_variable)
 
 from capstone.backend.llms.utils.splitter import SplitterManager
 
-# Need more time to plan this
+load_dotenv()
+
+"""
+################ TAKE NOTE #################
+#   
+#   Consept of This file is to manage all 
+#   of process that use with RAG.
+#
+#
+############################################
+"""
 
 # Design RAG system before do this one.
+
+## Define Class for Retrieval-Augmented Generation ( RAG. )
+class baseRAG(ABC):
+    pass
+
+## Load Document
+# Using LoaderManger to Handle PDF, Website, Text, API Upload.
+    def load(self):
+        pass
+        
+## Split text
+
+
+## Embedding
+
+
+## Stored VectorDB & Retreieval
+
+
+## LLMs
+
+
+## Generation
+
+
+## Performance Zone
+
+
+## Evaluation
+
+if __name__ == '__main__':
+
+    test = baseRAG()
---
+++ capstone/backend/llms/prompt_teamplate/README.md
@@ -0,0 +1,4 @@
+## PROMPT TEMPLATE
+
+# This Field is use for test Perfromance and Few-short for LLMs
+# Read more [Click me](https://www.promptingguide.ai/introduction/settings)
