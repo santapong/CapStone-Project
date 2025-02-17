18caeb4beac434ad919801511504c505840b5b7e
LAPTOP-UFCHV9CV\User <jobnakubphom@gmail.com>
2025-02-17 13:32:54 +0700

    Update Dependency

--- capstone/backend/database/events.py
+++ capstone/backend/database/events.py
@@ -2,24 +2,30 @@ import joblib
 import logging
 from sqlalchemy.sql import text
 from sqlalchemy.event import listens_for
+from capstone.backend.database import (
+    get_db, 
+    DBConnection
+    )
 from capstone.backend.database.models import (
-    ChatHistoryTable, 
+    LogsTable, 
     CategoryTable
     )
 
-from capstone.backend.database import get_db, DBConnection
-
-from sqlalchemy.orm import Session
 
 logging.getLogger(__name__)
 
-loaded_model = joblib.load('text_classification.pkl')
-logging.info("Downloaded Model Success")
-
+def get_model():
+    try:
+        logging.info("Downloaded Model Success")
+        yield joblib.load('text_classification.pkl')
+    except: 
+        raise FileNotFoundError("Cannot find Model location")
+    
 # TODO: Implement Event to Extract Summary from ChatHistory 
 # Event to Extract Summary from ChatHistory
-@listens_for(ChatHistoryTable, 'after_insert')
+@listens_for(LogsTable, 'after_insert')
 def categorize_question(mapper, connection, target):
     db: DBConnection = get_db()
-    category_data = loaded_model.predict([target.question])
+    model = get_model()
+    category_data = model.predict([target.question])
     db.insert(CategoryTable, question=target.question, category=category_data[0])
